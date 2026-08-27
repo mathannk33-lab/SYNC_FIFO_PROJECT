96419e8df3e235707f0ac365527c3b504951765b
mathan <mathannk33@gmail.com>
2026-08-27 11:52:21 +0530

    Create test_pkg.sv

---
+++ src/TestBench/test_pkg.sv
@@ -0,0 +1,19 @@
+package test_pkg;
+import uvm_pkg::* ;
+`include "uvm_macros.svh"
+`include "trans.sv"
+`include "fifo_config.sv"
+`include "input_driver.sv"
+`include "input_monitor.sv"
+`include "input_sequencer.sv"
+`include "input_agent.sv"
+`include "output_monitor.sv"
+`include "output_agent.sv"
+`include "scoreboard.sv"
+`include "subscriber.sv"
+`include "env.sv"
+`include "seq.sv"
+`include "test.sv"
+
+endpackage
+
